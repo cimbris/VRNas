c5521078c788840debe3bda33f0ab019d069cec9
Arseniy <arseniy.cimbris@gmail.com>
2025-06-06 15:14:23 +0300

    feat: testimonial adaptive done

--- src/common-scripts/heads.js
+++ src/common-scripts/heads.js
@@ -1,14 +1,27 @@
 const headList = document.querySelector("#headList");
+const items = document.querySelectorAll(".head-list__item");
 
 const click = (e) => {
     const isBtn = e.target.closest("[data-btn]");
+    const block = isBtn.closest(".head-list__item");
+
+    items.forEach((item) => {
+        item.classList.remove("active");
+    });
 
     if (isBtn) {
+        if (isBtn.classList.contains("active")) {
+            block.classList.remove("active");
+            isBtn.classList.remove("active");
+
+            return;
+        }
         const buttons = headList.querySelectorAll("[data-btn]");
         buttons.forEach((button) => {
             button.classList.remove("active");
         });
         isBtn.classList.toggle("active");
+        block.classList.toggle("active");
     }
 };
 
